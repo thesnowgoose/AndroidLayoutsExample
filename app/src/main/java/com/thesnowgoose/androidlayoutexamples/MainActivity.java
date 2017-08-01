5f77ce05be016bd40cd415383005f8b413e40d7e
Leonardo Carrasco <lcarrasco@nearsoft.com>
2017-08-01 16:27:34 -0600

    clear branch to start working on it

--- app/src/main/java/com/thesnowgoose/androidlayoutexamples/MainActivity.java
+++ app/src/main/java/com/thesnowgoose/androidlayoutexamples/MainActivity.java
@@ -9,26 +9,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout background;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        background = (LinearLayout) findViewById(R.id.background);
     }
 
-    public void onClicked(View view) {
-        String text = view.getId() == R.id.view1 ? "Green" : "Orange";
-
-        if (view.getId() == R.id.view1) {
-            background.setBackgroundColor(
-                    ResourcesCompat.getColor(getResources(), android.R.color.holo_green_light, null));
-        } else if (view.getId() == R.id.view2) {
-            background.setBackgroundColor(
-                    ResourcesCompat.getColor(getResources(), android.R.color.holo_orange_dark, null));
-        }
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-    }
 }
