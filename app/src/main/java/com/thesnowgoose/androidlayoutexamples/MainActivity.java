9cd634e68ffcea73a6efff540c6017f45928c323
Leonardo Carrasco <lcarrasco@nearsoft.com>
2017-08-01 05:40:18 -0600

    clear branch to start working on it

--- app/src/main/java/com/thesnowgoose/androidlayoutexamples/MainActivity.java
+++ app/src/main/java/com/thesnowgoose/androidlayoutexamples/MainActivity.java
@@ -10,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
